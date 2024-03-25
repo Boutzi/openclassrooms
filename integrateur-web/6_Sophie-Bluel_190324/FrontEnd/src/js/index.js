95dcf5e8fc086d47419b3671f8ff50a1ce7c20e3
Boutzi <joe@arkaans.com>
2024-03-25 18:41:28 +0100

    fix: links for GitHub Pages

--- integrateur-web/6_Sophie-Bluel_190324/FrontEnd/src/js/index.js
+++ integrateur-web/6_Sophie-Bluel_190324/FrontEnd/src/js/index.js
@@ -28,9 +28,9 @@ if (token) {
     });
 }
 
-const works = await fetch("https://sophie-bluel-api-5461e3d9daac.herokuapp.com/api/works").then((response) => response.json());
+const works = await fetch("https://sophie-bluel-api-aa2d8b8c980b.herokuapp.com/api/works").then((response) => response.json());
 
-const categories = await fetch("https://sophie-bluel-api-5461e3d9daac.herokuapp.com/api/categories").then((response) =>
+const categories = await fetch("https://sophie-bluel-api-aa2d8b8c980b.herokuapp.com/api/categories").then((response) =>
   response.json()
 );
 
