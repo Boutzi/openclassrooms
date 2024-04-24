f850269fc19e3255d9d88bec24eb61e493a0febb
Boutzi <joe@arkaans.com>
2024-04-24 12:30:50 +0200

    feat(P7): adding React Router

--- integrateur-web/7_Kasa_180424/Kasa/src/main.jsx
+++ integrateur-web/7_Kasa_180424/Kasa/src/main.jsx
@@ -1,12 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./components/Home";
-import Footer from "./components/Footer";
-import "./styles/main.css";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Footer from "./components/Footer"
+import Error from "./components/Error";
+import "./styles/main.scss"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
-    <Footer />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
   </React.StrictMode>
-);
+)
